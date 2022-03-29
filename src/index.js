c72dccb96ee758c25a98a4521d58de4e41164137
yuha <the.zihuatanejo@gmail.com>
2022-03-29 23:54:20 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+/**
+ * const,letの変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は際宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // constは上書き不可能
+// val3 = "const変数を上書き";
+
+// // const変数は再宣言不可能
+// const val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "yuya",
+//   age: 20
+// };
+// // console.log(val4);
+
+// val4.name = "ゆうや";
+// // console.log(val4);
+
+// val4.address = "Tokyo";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+const name = "ゆうや";
+const age = 34;
+//「私の名前はゆうやです。年齢は34歳です」
